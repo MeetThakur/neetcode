f00d84b14774331aefb796fe9af0413e341abb44
Meet Thakur <meet11india@gmail.com>
2026-07-05 00:14:29 +0530

    Add: islands-and-treasure - submission-1

---
+++ Data Structures & Algorithms/islands-and-treasure/submission-1.py
@@ -0,0 +1,36 @@
+from collections import deque
+from typing import List
+
+class Solution:
+    def islandsAndTreasure(self, grid: List[List[int]]) -> None:
+
+        ROWS, COLS = len(grid), len(grid[0])
+
+        def find(i, j):
+            q = deque([(i, j, 0)])
+            visited = {(i, j)}
+
+            while q:
+                r, c, d = q.popleft()
+
+                if grid[r][c] == 0:
+                    return d
+
+                for dr, dc in [(1,0), (-1,0), (0,1), (0,-1)]:
+                    nr, nc = r + dr, c + dc
+
+                    if (
+                        0 <= nr < ROWS and
+                        0 <= nc < COLS and
+                        grid[nr][nc] != -1 and
+                        (nr, nc) not in visited
+                    ):
+                        visited.add((nr, nc))
+                        q.append((nr, nc, d + 1))
+
+            return 2147483647
+
+        for i in range(ROWS):
+            for j in range(COLS):
+                if grid[i][j] == 2147483647:
+                    grid[i][j] = find(i, j)
